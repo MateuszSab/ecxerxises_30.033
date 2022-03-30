2f1c649103a9676f37b1a5080246a347e0611b5a
Jacek Laskowski <jacek@japila.pl>
2022-03-30 13:15:59 +0200

    New tests

--- src/test/scala/SeqCounterSpec.scala
+++ src/test/scala/SeqCounterSpec.scala
@@ -7,12 +7,16 @@ import SeqCounter._
 class SeqCounterSpec extends AnyFlatSpec with should.Matchers {
 
   // RTFM: https://www.scalatest.org/user_guide/writing_your_first_test
-  "My Great Suite" should "???" in {
+  "seqCounter" should "check if every two, three, n numbers in a sequence are followed by their sum" in {
     val listgood = Seq(1, 2, 3, 6, 0, 0, 0, 0, 1, 1, 1, 3)
-    val listbad = Seq(1, 2, 3, 5, 0, 0, 0, 1, 1, 1, 1, 4)
-    val listempty = Seq()
     seqCounter(listgood, 3) shouldBe true
+
+    val listbad = Seq(1, 2, 3, 5, 0, 0, 0, 1, 1, 1, 1, 4)
     seqCounter(listbad, 3) shouldBe false
+  }
+
+  it should "return true for empty list" in {
+    val listempty = Seq()
     seqCounter(listempty, 3) shouldBe true
   }
 }
