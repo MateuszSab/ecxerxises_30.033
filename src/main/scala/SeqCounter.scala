4644f7eaf3e1a7602dda0d145d3a1c88031a9491
Jacek Laskowski <jacek@japila.pl>
2022-03-30 13:21:26 +0200

    Refactor + tailrec

--- src/main/scala/SeqCounter.scala
+++ src/main/scala/SeqCounter.scala
@@ -1,14 +1,16 @@
+import scala.annotation.tailrec
+
 object SeqCounter {
 
+  @tailrec
   def seqCounter(ns: Seq[Int], n: Int): Boolean = {
 
     def nth(ns: Seq[Int], n: Int): Option[Int] = {
       ns.drop(n).headOption
     }
 
-    val x = ns.take(n)
     if (ns.isEmpty) true
-    else if (Option(x.sum) == nth(ns, n)) seqCounter(ns.drop(n + 1), n)
+    else if (nth(ns, n).contains(ns.take(n).sum)) seqCounter(ns.drop(n + 1), n)
     else false
   }
 }
